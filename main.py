26224f79281475d757bbb97dde73b6c11d24c67b
Mohammed Usama <mohammedsimsim17@gmail.com>
2026-07-25 13:51:25 +0300

    Source code move to a separate text file

--- main.py
+++ main.py
@@ -1,219 +1,25 @@
 import sys
+from time import perf_counter_ns
+
 from my_compiler import Compiler
 from my_lexer import *
-from time import perf_counter_ns
 from my_parser import Parser
 from my_virtual_machine import VirtualMachine
 
 
 sys.set_int_max_str_digits(1000000)
 # ================== Source Code ===================
-source = '''
-/*
-var x = 1555.5;
-var str = "hello";
-var test = 55 > 0;
-var testt = 55 <= 0;
-var y = 66;
+with open("source.txt", "r", encoding="utf-8") as source_file:
+    source_code = source_file.read()
 
-func add(a, b) {
-    return a + b;
-}
-
-var sum = add(x, 3);
-
-x = 5;
-x ++;
-y --;
-x *= 8;
-y ^= 5;
-
-if (9 >= x) {
-    if (x != 4) {
-        y += 7; 
-        x --;
-    }
-
-    if (y == 7) {
-        y --;
-    }
-}
-
-while (x >= 8) {
-    x --;
-    if (x == 6) {
-        continue;
-    }
-    
-    while (x > 50) {
-        x++;
-        break;
-    }
-    
-    if (y != x^2) {
-        break;
-    }
-    y ++;
-}
-
-func mul(a, b) {
-    var result = 0;
-    
-    func myAdd(a, b) {
-        var myAddResult = a + b;
-        result ++;
-        result --;
-        x++;
-        return myAddResult;
-    }
-    
-    while (b > 0) {
-        result = myAdd(result, a);
-        b --;
-    }
-    
-    if (b < 0) {
-        return "Unexpected error";
-    }
-    
-    return result;
-}
-
-//A comment
-var long = """
-hello, how are you""";
-
-var opp = -(-add(2, 3));
-
-
-func recursiveMul(a, b) {
-    if (b <= 0) {
-        return 0;
-    }
-    
-    return a + recursiveMul(a, b - 1);
-}
-
-var tetra = tetration(7, 2);
-tetra;
-
-var multi = mul(3, 4);
-multi;
-
-var reMulti = recursiveMul(12, 12);
-reMulti;
-
-log(3 + 4);
-log(reMulti + tetra);
-*/
-
-
-func tetration(a, b) {
-    var result = 1;
-    
-    while (b > 0) {
-        result = a ^ result;
-        b--;
-    }
-    
-    return result;
-}
-
-func main() {
-    var status = True;
-    
-    while (status) {
-        var firstNum = Integer(input("Enter 1st number: "));
-        
-        while (True) {
-            var operator = input("Enter the operator: ");
-            
-            if (operator != "+") {
-                if (operator != "-") {
-                    if (operator != "*") {
-                        if (operator != "/") {
-                            if (operator != "**") {
-                                if (operator != "tetration") {
-                                    log("Invalid operator, please enter another valid operator ( + | - | * | / )");
-                                    continue;
-                                }
-                                break;
-                            }
-                            break;
-                        }   
-                        break;
-                    }
-                    break;
-                }
-                break;
-            }
-            break;
-        }
-        
-        var secondNum = Integer(input("Enter 2nd number: "));
-        
-        var result = Null;
-        
-        if (operator == "+") {
-            result = firstNum + secondNum;
-        }
-        if (operator == "-") {
-            result = firstNum - secondNum;
-        }
-        if (operator == "*") {
-            result = firstNum * secondNum;
-        }
-        if (operator == "/") {
-            if (secondNum == 0) {
-                log("Cannot divide by 0, enter another number");
-                continue;
-            }
-            result = firstNum / secondNum;
-        }
-        if (operator == "**") {
-            result = firstNum ^ secondNum;
-        }
-        if (operator == "tetration") {
-            result = tetration(firstNum, secondNum);
-        }
-        
-        
-        if (result == Null) {
-            continue;
-        }
-        
-        log(result);
-                
-        while (True) {
-            var choice = input("do you want to perform another operation ? (Y/N): ");
-            
-            if (choice == "Y") {
-                break;
-            }
-            if (choice == "N") {
-                status = False;
-                break;
-            }
-            if (choice != "Y") {
-                if (choice != "N") {
-                    log("Invalid choice, please enter (Y/N) letters only");       
-                }
-            }
-        }
-    }
-
-}
-
-main();
-'''
 start = perf_counter_ns()
 # print(" Source Code ".center(100, "="))
-# print(source)
+# print(source_code)
 # ==================================================
 
 
 # ===================== Lexer ======================
-tokens = Lexer(source).scan()
+tokens = Lexer(source_code).scan()
 
 # print("\n" + " Tokens ".center(100, "="))
 # for index, token in enumerate(tokens):
